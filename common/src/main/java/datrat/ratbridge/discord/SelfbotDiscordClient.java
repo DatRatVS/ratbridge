74c94864f709b11f6f30a1b816504b27a249641b
DatRatVS <ricorian2006@gmail.com>
2026-06-14 12:47:33 -0300

    chore: bump version to 0.1.8

--- common/src/main/java/datrat/ratbridge/discord/SelfbotDiscordClient.java
+++ common/src/main/java/datrat/ratbridge/discord/SelfbotDiscordClient.java
@@ -197,7 +197,7 @@ public final class SelfbotDiscordClient implements DiscordBridgeClient {
     private HttpRequest.Builder baseRequest(URI uri) {
         return HttpRequest.newBuilder(uri)
                 .header("Authorization", token)
-                .header("User-Agent", "RatBridge/0.1.7");
+                .header("User-Agent", "RatBridge/0.1.8");
     }
 
     private URI channelUri(String suffix) {
